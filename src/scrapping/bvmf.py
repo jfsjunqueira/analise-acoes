d929a68e979bb58d6dfd6aa41f92848ce224a779
jfsjunqueira <jfsjunqueira@gmail.com>
2022-11-22 13:55:33 -0300

    update: Refactored BVMF scrapping to user pandas.

--- src/scrapping/bvmf.py
+++ src/scrapping/bvmf.py
@@ -5,11 +5,12 @@ from typing import List
 
 import requests
 from bs4 import BeautifulSoup
+from pandas import DataFrame, read_html
 
 from src.config import BVMF_URL
 
 
-def bvmf_scrap(cd_cvm: str) -> dict:
+def bvmf_scrap(cd_cvm: str) -> List[DataFrame]:
     """Scrapes certain characteristics about a company based on its
     CVM code.
 
@@ -17,83 +18,15 @@ def bvmf_scrap(cd_cvm: str) -> dict:
         cd_cvm (str): CVM code referent to a specific company.
 
     Returns:
-        dict: Dictionary with informations about a specific company.
+        table_list: List with tables containing informations about a specific company.
+            [0] - General Information
+            [1] - Institution
+            [2] - Balance sheet summary for last 2 years YTD
+            [3] - Income statement summary for last 2 years YTD
+            [4] - Cashflow summary for last 2 years YTD
+            [5] - Shareholders
+            [6] - Types of shareholders
+            [7] - Types of shares
+            
     """
-    bvmf_page = BeautifulSoup(get_page(cd_cvm).text, 'html.parser')
-    ticker_list = scrap_ticker_list(bvmf_page)
-    industry = scrap_industry(bvmf_page)
-    n_shares = scrap_total_shares(bvmf_page)
-    company_dict = {
-        "ticker": ticker_list,
-        "activity": [industry[0]] * len(ticker_list),
-        "industry": [industry[1]] * len(ticker_list),
-        "ord_n": [n_shares[0]] * len(ticker_list),
-        "pre_n": [n_shares[1]] * len(ticker_list),
-    }
-    return company_dict
-
-
-def get_page(cd_cvm: str, bvmf_url=BVMF_URL) -> requests.models.Response:
-    """Gets the BVMF page html code for the specified CD_CVM.
-
-    Args:
-        cd_cvm (str): CD_CVM that is used as ID for companies in CVM files.
-        bvmf_url (str, optional): BVMF base website URL with information about the company. Defaults to BVMF_URL.
-
-    Returns:
-        requests.models.Response: Reponse object from the request.
-    """
-    return requests.get(bvmf_url + cd_cvm)
-
-
-def scrap_ticker_list(bvmf_page: BeautifulSoup) -> List:
-    """Scrapes the tickers of a company's page in BVMF.
-
-    Args:
-        bvmf_page (BeautifulSoup): BS4 object of the company's html code.
-
-    Returns:
-        List: List with all of the company's tickers.
-    """
-    raw_ticker_list = bvmf_page.find_all(class_="LinkCodNeg")
-    return [ticker.contents[0] for ticker in raw_ticker_list]
-
-
-def scrap_industry(bvmf_page: BeautifulSoup) -> List:
-    """Scrapes the industry and sector especifications in a company's page in BVMF.
-
-    Args:
-        bvmf_page (BeautifulSoup): BS4 object of the company's html code.
-
-    Returns:
-        List: List with (1) Main activity and (2) Industry specification of the company.
-    """
-    raw_industry_list = bvmf_page.find(class_="ficha responsive").find_all("td")[-5:]
-    return [raw_industry_list[0].contents[0], raw_industry_list[2].contents[0]]
-
-
-def scrap_total_shares(bvmf_page: BeautifulSoup) -> List:
-    """Scrapes the number of ordinary and preferential shares of a company's page
-    in BVMF website.
-
-    Args:
-        bvmf_page (BeautifulSoup): BS4 object of the company's html code.
-
-    Returns:
-        List: List with (1) Number of ordinary and (2) preferential shares.
-    """
-    ord_n = (
-        bvmf_page.find(text=re.compile("Quantidade de AÃ§Ãµes Ord"))
-        .findParent()
-        .find_next_sibling()
-        .contents[0]
-        .replace(".", "")
-    )
-    pre_n = (
-        bvmf_page.find(text=re.compile("Quantidade de AÃ§Ãµes Pre"))
-        .findParent()
-        .find_next_sibling()
-        .contents[0]
-        .replace(".", "")
-    )
-    return [ord_n, pre_n]
+    return read_html(BVMF_URL + str(cd_cvm))
