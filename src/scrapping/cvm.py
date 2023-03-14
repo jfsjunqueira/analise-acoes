84ec015988737a065c359439d1cacf7a4e623c05
Joao Junqueira <jfsjunqueira@gmail.com>
2023-03-14 11:42:48 -0300

    fix: fixed problem with download_cvm task.

--- src/scrapping/cvm.py
+++ src/scrapping/cvm.py
@@ -2,40 +2,41 @@ import os
 import zipfile
 
 import requests
-
+from pathlib import Path
+from src.base_logger import logger
 from src.scrapping.checks import check_already_downloaded
 
 
-def download_unzip(url: str, dest_folder: str) -> None:
-    """Downloads and unzips file from target url to destination folder.
+def download_unzip(url: str, dest_folder: Path) -> None:
+    """Downloads and unzips file from target usrl to destination folder.
 
     Args:
         url (str): URL of the target file to be downloaded.
-        dest_folder (str): Path to destination folder where file should be unziped.
+        dest_folder (pathlib.Path): Path to destination folder where file should be unziped.
     """
-    print("Downloading the file...")
-    file_name = download_file(url, dest_folder)
-    print("Unzipping the file...")
-    unzip_file(file_name, dest_folder=file_name[:-4])
-    print(f"File {file_name} successfuly downloaded to {dest_folder}!")
+    logger.info("Downloading the file...")
+    file_name = Path(download_file(url, dest_folder))
+    logger.info("Unzipping the file...")
+    unzip_file(file_name, dest_folder=file_name.parents[0].joinpath(file_name.stem))
+    logger.info(f"File {file_name} successfuly downloaded to {dest_folder}!")
 
 
-def create_if_not_dir(dest_folder: str) -> None:
+def create_if_not_dir(dest_folder: Path) -> None:
     """Creates a directory in dest_folder PATH if it doesn't exist yet.
 
     Args:
-        dest_folder (str): Path to destination folder.
+        dest_folder (pathlib.Path): Path to destination folder.
     """
     if not os.path.exists(dest_folder):
         os.makedirs(dest_folder)
 
 
-def download_file(url: str, dest_folder: str) -> str:
+def download_file(url: str, dest_folder: Path) -> str:
     """Downloads file from a specified URL to a specified destination folder.
 
     Args:
         url (str): URL with the file to be downloaded.
-        dest_folder (str): Path of the destination folder.
+        dest_folder (pathlib.Path): Path of the destination folder.
 
     Returns:
         str: File path of the downloaded file.
@@ -45,12 +46,12 @@ def download_file(url: str, dest_folder: str) -> str:
     filename = url.split("/")[-1].replace(" ", "_")
     file_path = os.path.join(dest_folder, filename)
     if check_already_downloaded(file_path):
-        print(f"Error: File {os.path.abspath(file_path)} already exists.")
+        logger.error(f"File {os.path.abspath(file_path)} already exists.")
         return file_path
     r = requests.get(url, stream=True)
 
     if r.ok:
-        print(f"Success: Saving to {os.path.abspath(file_path)}")
+        logger.info(f"Saving to {os.path.abspath(file_path)}")
         with open(file_path, "wb") as f:
             for chunk in r.iter_content(chunk_size=1024 * 8):
                 if chunk:
@@ -58,18 +59,21 @@ def download_file(url: str, dest_folder: str) -> str:
                     f.flush()
                     os.fsync(f.fileno())
     else:
-        print(f"Error: Download failed status code {r.status_code}\n{r.text}")
+        logger.error(f"Download failed status code {r.status_code}\n{r.text}")
     return file_path
 
 
-def unzip_file(file_path: str, dest_folder: str):
+def unzip_file(file_path: Path, dest_folder: Path):
     """Unzips a .zip file to a specified destination folder.
 
     Args:
         file_path (str): Path of the .zip file.
-        dest_folder (str): Destination folder where the unziped files should be stored.
+        dest_folder (pathlib.Path): Destination folder where the unziped files should be stored.
     """
-    create_if_not_dir(dest_folder)
-    with zipfile.ZipFile(file_path, "r") as zip_ref:
-        zip_ref.extractall(f"./{dest_folder}")
+    if dest_folder.exists():
+        logger.error(f"Folder {os.path.abspath(dest_folder)} already exists.")
+    else:
+        create_if_not_dir(dest_folder)
 
+    with zipfile.ZipFile(file_path, "r") as zip_ref:
+        zip_ref.extractall(f"{dest_folder}")
--- tasks/download_cvm.py
+++ tasks/download_cvm.py
@@ -1,30 +1,37 @@
 """ Setting actual wd for task """
+if __name__ == "__main__" and __package__ is None:
+    from os import sys, path  # type: ignore
 
-if __name__ == '__main__' and __package__ is None:
-    from os import sys, path
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 
 """ Actual task """
-
+from src.config import CVM_URL, DATA_PATH
 from src.scrapping.cvm import download_unzip
 from src.base_logger import logger
-from src.config import CVM_URL, DATA_PATH
 from datetime import datetime
 from tqdm import tqdm
+import pathlib
 
+STARTING_YEAR = 2020
 CURRENT_YEAR = datetime.now().year
+TARGET_DIRECTORY = pathlib.Path(__file__).parents[1].joinpath(DATA_PATH)
+
 
 def main():
-    url_list = [f'{CVM_URL[:-8]}{i}.zip' for i in range(2011,CURRENT_YEAR-1,1)]
+    url_list = [f"{CVM_URL[:-8]}{i}.zip" for i in range(STARTING_YEAR, CURRENT_YEAR - 1, 1)]
 
-    for url in tqdm(url_list, 'Downloading DFPs'):
-        download_unzip(url, '../'+DATA_PATH)
+    for url in tqdm(url_list, "Downloading DFPs"):
+        download_unzip(url, TARGET_DIRECTORY)
 
 
-while __name__ == "__main__": 
-    logger.info(f"Starting to download DFPs for dates between and including 2011 and {CURRENT_YEAR - 1}")
+while __name__ == "__main__":
+    logger.info(
+        f"Starting to download DFPs for dates between and including 2011 and {CURRENT_YEAR - 1}"
+    )
     main()
     logger.info(f"CVM data succesfully downloaded!")
-    logger.info(f"You can find everything inside the 'data' folder, on this project root directory.")
+    logger.info(
+        f"You can find everything inside the 'data' folder, on this project root directory."
+    )
     break
